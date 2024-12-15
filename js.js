3ef8761e86c0bdbf08b57bfec8b99988879b86e2
sunitha <gsita3251@gmail.com>
2024-12-15 15:56:03 +0530

    files created

---
+++ js.js
@@ -0,0 +1,8 @@
+let tags=document.getElementById("tags")
+tags.innerHTML="<img src='https://images.meesho.com/images/products/85630166/cisnh_400.webp'width='300px'/><p>300rs</p><p>GRT diamonds</p><button>Buy now</button>"
+console.log(tags)
+
+
+let tags2=document.getElementById("tags2")
+tags2.innerHTML="<img src='https://images.meesho.com/images/products/399149217/eroum_400.webp'width='300px'/><p>500rs</p><p>kid's wear</p><button>buy now</button>"
+console.log(tags2)
